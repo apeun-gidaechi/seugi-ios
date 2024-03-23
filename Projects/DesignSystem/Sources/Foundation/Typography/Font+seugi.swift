4b644f63c5e6cb30667997096d547ae81c28f711
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-03-23 11:19:43 +0900

    stlye : Font 코드 스타일 수정

--- Projects/DesignSystem/Sources/Foundation/Typography/Font+seugi.swift
+++ Projects/DesignSystem/Sources/Foundation/Typography/Font+seugi.swift
@@ -13,7 +13,7 @@ public extension Font {
         case caption1
         case caption2
     }
-
+    
     static func seugi(_ style: SeugiFontSystem) -> Font {
         return style.font
     }
@@ -22,16 +22,16 @@ public extension Font {
 public extension Font.SeugiFontSystem {
     var font: Font {
         switch self {
-        case .display1: Font(DesignSystemFontFamily.Pretendard.bold.font(size: 36))
-        case .display2: Font(DesignSystemFontFamily.Pretendard.bold.font(size: 32))
-        case .title1: Font(DesignSystemFontFamily.Pretendard.bold.font(size: 28))
-        case .title2: Font(DesignSystemFontFamily.Pretendard.bold.font(size: 24))
-        case .subtitle1: Font(DesignSystemFontFamily.Pretendard.semiBold.font(size: 20))
-        case .subtitle2: Font(DesignSystemFontFamily.Pretendard.semiBold.font(size: 16))
-        case .body1: Font(DesignSystemFontFamily.Pretendard.semiBold.font(size: 14))
-        case .body2: Font(DesignSystemFontFamily.Pretendard.regular.font(size: 14))
-        case .caption1: Font(DesignSystemFontFamily.Pretendard.semiBold.font(size: 12))
-        case .caption2: Font(DesignSystemFontFamily.Pretendard.regular.font(size: 12))
+        case .display1: DesignSystemFontFamily.Pretendard.bold.swiftUIFont(size: 36)
+        case .display2: DesignSystemFontFamily.Pretendard.bold.swiftUIFont(size: 32)
+        case .title1: DesignSystemFontFamily.Pretendard.bold.swiftUIFont(size: 28)
+        case .title2: DesignSystemFontFamily.Pretendard.bold.swiftUIFont(size: 24)
+        case .subtitle1: DesignSystemFontFamily.Pretendard.semiBold.swiftUIFont(size: 20)
+        case .subtitle2: DesignSystemFontFamily.Pretendard.semiBold.swiftUIFont(size: 16)
+        case .body1: DesignSystemFontFamily.Pretendard.semiBold.swiftUIFont(size: 14)
+        case .body2: DesignSystemFontFamily.Pretendard.regular.swiftUIFont(size: 14)
+        case .caption1: DesignSystemFontFamily.Pretendard.semiBold.swiftUIFont(size: 12)
+        case .caption2: DesignSystemFontFamily.Pretendard.regular.swiftUIFont(size: 12)
         }
     }
 }
