074a0912c758899591a5b4edd49f02cd2d8cb579
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-03-23 12:31:04 +0900

    feat : if View extension 추가

--- Projects/DesignSystem/Sources/Extensions/View+.swift
+++ Projects/DesignSystem/Sources/Extensions/View+.swift
@@ -2,6 +2,21 @@ import SwiftUI
 
 extension View {
     func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
-        clipShape( RoundedCorner(radius: radius, corners: corners) )
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+extension View {
+    /// Applies the given transform if the given condition evaluates to `true`.
+    /// - Parameters:
+    ///   - condition: The condition to evaluate.
+    ///   - transform: The transform to apply to the source `View`.
+    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
     }
 }
