e1f9ffb9288730487d6441bf5a90ca19f9c6734e
bswlkh0310 <hhhello0507@gmail.com>
2024-03-28 08:54:41 +0900

    example : 디자인 시스템 Example 앱 추가

---
+++ Projects/UserInterface/DesignSystem/Example/메ㅔ/DesignSystemApp.swift
@@ -0,0 +1,18 @@
+//
+//  DesignSystemApp.swift
+//  DesignSystem
+//
+//  Created by dgsw8th71 on 3/28/24.
+//  Copyright © 2024 apeun.gidaechi. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct DesignSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SeugiPreview.preview
+        }
+    }
+}
