7db1dee2c52a014d10a141d461f7e763eb19bb98
bswlkh0310 <hhhello0507@gmail.com>
2024-03-27 15:58:10 +0900

    feat : 초대 코드 오지 않을 때 대처하는 버튼 추가

--- Projects/Feature/JoinSchoolFeature/Sources/JoinSchoolView.swift
+++ Projects/Feature/JoinSchoolFeature/Sources/JoinSchoolView.swift
@@ -18,10 +18,17 @@ public struct JoinSchoolView: View {
     public init() {}
     
     public var body: some View {
-        VStack {
+        VStack(spacing: 16) {
             SeugiTextFieldForm("초대코드를 입력해 주세요", text: $vm.code, label: "초대코드")
                 .padding(.top, 16)
             Spacer()
+            Button {
+                
+            } label: {
+                Text("초대 초드가 오지 않는다면?")
+                    .seugiForeground(.primary(.p500))
+                    .font(.seugi(.body1))
+            }
             SeugiButton("계속하기", type: .primary) {
                 // TODO: handle join school
             }
