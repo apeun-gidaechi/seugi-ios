6eb490ae042a3afa085833253c9d79994656bf3f
bswlkh0310 <hhhello0507@gmail.com>
2024-03-28 22:18:46 +0900

    example : Chat Detail 예제 뷰 추가

---
+++ Projects/Feature/ChatDetailFeature/Example/ChatDetailViewExample.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import ChatDetailFeature
+
+@main
+struct ChatDetailViewExample: App {
+    var body: some Scene {
+        WindowGroup {
+            ChatDetailView()
+        }
+    }
+}
--- Projects/Feature/ChatDetailFeature/Sources/ChatDetailView.swift
+++ Projects/Feature/ChatDetailFeature/Sources/ChatDetailView.swift
@@ -1,14 +1,23 @@
 import SwiftUI
+import DesignSystem
 
 public struct ChatDetailView: View {
+    
+    @Environment(\.dismiss) private var dismiss
     
     public init() {}
     
     public var body: some View {
         ScrollView {
             VStack {
-                
+                ChatItemDateView(date: "2024년 3월 21일 목요일")
+                ChatItemView(author: "이강현", type: .other)
+                ChatItemView(author: "이강현", type: .me)
+                ChatItemView(author: "박재욱", type: .other)
             }
+        }
+        .seugiToolbar("노영재") {
+            dismiss()
         }
     }
 }
