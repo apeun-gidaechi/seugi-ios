0109c4fb2ea496c4387c3cfdc51cb7d694143b98
bswlkh0310 <hhhello0507@gmail.com>
2024-03-28 22:14:21 +0900

    feat : Chat Item Date 컴포넌트 추가

---
+++ Projects/UserInterface/DesignSystem/Sources/Component/ChatItem/ChatItemDateView.swift
@@ -0,0 +1,32 @@
+//
+//  ChatItemDateView.swift
+//  DesignSystem
+//
+//  Created by dgsw8th71 on 3/28/24.
+//  Copyright © 2024 apeun.gidaechi. All rights reserved.
+//
+
+import SwiftUI
+
+public struct ChatItemDateView: View {
+    
+    var date: String
+    
+    public init(date: String) {
+        self.date = date
+    }
+    
+    public var body: some View {
+        HStack {
+            Spacer()
+            Text(date)
+                .font(.seugi(.caption2))
+                .padding(.horizontal, 16)
+                .padding(.vertical, 8)
+                .background(Color.seugi(.gray(.g100)))
+                .cornerRadius(24, corners: .allCorners)
+                .padding(.vertical, 12)
+            Spacer()
+        }
+    }
+}
