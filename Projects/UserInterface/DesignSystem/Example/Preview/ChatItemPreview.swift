e7e3ae99998cb57fcc509ba45e066159225a06b0
bswlkh0310 <hhhello0507@gmail.com>
2024-03-28 22:15:04 +0900

    example : Chat 프리뷰 추가

---
+++ Projects/UserInterface/DesignSystem/Example/Preview/ChatItemPreview.swift
@@ -0,0 +1,27 @@
+//
+//  ChatItemPreview.swift
+//  DesignSystem
+//
+//  Created by dgsw8th71 on 3/28/24.
+//  Copyright © 2024 apeun.gidaechi. All rights reserved.
+//
+
+import SwiftUI
+import DesignSystem
+
+public struct ChatItemPreview: View {
+    
+    public init() {}
+    
+    public var body: some View {
+        ScrollView {
+            VStack(spacing: 0) {
+                ChatItemDateView(date: "2024년 3월 21일 목요일")
+                ChatItemView(author: "이강현", type: .other)
+                ChatItemView(author: "이강현", type: .me)
+                ChatItemView(author: "박재욱", type: .other)
+            }
+        }
+    }
+}
+
