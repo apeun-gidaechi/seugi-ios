8c1894523141e642f07379405c95bd0490b594a1
bswlkh0310 <hhhello0507@gmail.com>
2024-03-30 20:54:59 +0900

    build : Main, Chat Tuist의존관계 재설정 #28

--- Projects/Feature/ChatFeature/Project.swift
+++ Projects/Feature/ChatFeature/Project.swift
@@ -5,8 +5,7 @@ let project = Project.makeFeature(
     target: .ChatFeature,
     targets: [
         .makeFeature(target: .ChatFeature, dependencies: [
-            .feature(target: .BaseFeature),
-            .feature(target: .ChatDetailFeature)
+            .feature(target: .BaseFeature)
         ]),
         .makeFeatureExample(target: .ChatFeature, dependencies: [
             .feature(target: .ChatFeature)
