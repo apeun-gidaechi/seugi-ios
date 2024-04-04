df357e5ebb91461f1672e795d441115e19572eed
bswlkh0310 <hhhello0507@gmail.com>
2024-04-04 18:24:25 +0900

    feat : scroll to bottom when it init

--- Projects/Feature/ChatDetailFeature/Sources/ChatDetailView.swift
+++ Projects/Feature/ChatDetailFeature/Sources/ChatDetailView.swift
@@ -1,28 +1,56 @@
 import SwiftUI
 import DesignSystem
 
+enum ChatType: Hashable {
+    case chat(id: Int)
+    case bottom
+}
+
 public struct ChatDetailView: View {
     
     @Environment(\.dismiss) private var dismiss
     @State private var text = ""
     @State private var isDrawerOpen = false
-    
+    @State private var scrollViewProxy: ScrollViewProxy?
+
     public init() {}
     
     public var body: some View {
         ZStack {
-            ScrollView {
-                VStack {
-                    ChatItemDateView(date: "2024년 3월 21일 목요일")
-                    ChatItemView(author: "이강현", type: .other)
-                    ChatItemView(author: "이강현", type: .me)
-                    ChatItemView(author: "박재욱", type: .other)
+            ScrollViewReader { scrollViewProxy in
+                ScrollView {
+                    LazyVStack {
+                        ChatItemDateView(date: "2024년 3월 21일 목요일").id(ChatType.chat(id: 1))
+                        ChatItemView(author: "이강현", type: .other).id(ChatType.chat(id: 2))
+                        ChatItemView(author: "이강현", type: .me).id(ChatType.chat(id: 3))
+                        ChatItemView(author: "박재욱", type: .other).id(ChatType.chat(id: 4))
+                        ChatItemView(author: "박재욱", type: .other).id(ChatType.chat(id: 5))
+                        ChatItemView(author: "박재욱", type: .other).id(ChatType.chat(id: 6))
+                        ChatItemView(author: "박재욱", type: .other).id(ChatType.chat(id: 7))
+                        ChatItemView(author: "박재욱", type: .other).id(ChatType.chat(id: 8))
+                        ChatItemView(author: "박재욱", type: .other).id(ChatType.chat(id: 9))
+                        ChatItemView(author: "박재욱", type: .other).id(ChatType.chat(id: 10))
+                        ChatItemView(author: "박재욱", type: .other).id(ChatType.chat(id: 11))
+                        Color.clear
+                            .frame(height: 52)
+                            .id(ChatType.bottom)
+                    }
+                    .onAppear {
+                        self.scrollViewProxy = scrollViewProxy
+                        scrollViewProxy.scrollTo(ChatType.bottom)
+                    }
                 }
             }
+            .background(Color.seugi(.primary(.p050)))
             SeugiChatTextField("메세지 보내기", text: $text) {
                 // handle more
             } sendButtonTapped: {
                 // handle send message
+                if let scrollViewProxy {
+                    withAnimation(.easeInOut(duration: 0.5)) {
+                        scrollViewProxy.scrollTo(ChatType.bottom)
+                    }
+                }
             }
             .toBottom()
             .padding(.horizontal, 8)
