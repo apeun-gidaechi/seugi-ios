ed864d62b48dba4db165668cfa40f140f4d29a79
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-04-07 02:09:58 +0900

    feat : SeugiIconography 추가

---
+++ Projects/UserInterface/DesignSystem/Sources/Foundation/Iconography/SeugiIconography.swift
@@ -0,0 +1,68 @@
+//
+//  SeugiIconography.swift
+//  DesignSystem
+//
+//  Created by dgsw8th71 on 4/7/24.
+//  Copyright © 2024 apeun.gidaechi. All rights reserved.
+//
+
+import SwiftUI
+
+public enum SeugiIconography: String {
+    
+    case addFill = "add_fill"
+    case addLine = "add_line"
+    case addRingLine = "add_ring_line"
+    
+    case arrowDownLine = "arrow_down_line"
+    case arrowLeftLine = "arrow_left_line"
+    case arrowRightLine = "arrow_right_line"
+    case arrowUpLine = "arrow_up_line"
+    
+    case chatFill = "chat_fill"
+    
+    case checkFill = "check_fill"
+    case checkLine = "check_line"
+    
+    case closeFill = "close_fill"
+    case closeLine = "close_line"
+    case closeRingLine = "close_ring_line"
+    
+    case detailHorizontalLine = "detail_horizontal_line"
+    case detailVerticalLine = "detail_vertical_line"
+    
+    case expandDownLine = "expand_down_line"
+    case expandLeftLine = "expand_left_line"
+    case expandRightLine = "expand_right_line"
+    case expandUpLine = "expand_up_line"
+    
+    case expandStopDownLine = "expand_stop_down_line"
+    case expandStopLeftLine = "expand_stop_left_line"
+    case expandStopRightLine = "expand_stop_right_line"
+    case expandStopUpLine = "expand_stop_up_line"
+    
+    case hamburgerHorizontalLine = "hamburger_horizontal_line"
+    
+    case showFill = "show_fill"
+    case hideFill = "hide_fill"
+    
+    case homeFill = "home_fill"
+    
+    case imageLine = "image_line"
+    
+    case logoutLine = "logout_line"
+    
+    case notificationDisabledFill = "notification_disabled_fill"
+    case notificationFill = "notification_fill"
+    
+    case peopleFill = "people_fill"
+    case personFill = "person_fill"
+    
+    case schoolFill = "school_fill"
+    
+    case searchLine = "search_line"
+    
+    case sendFill = "send_fill"
+    
+    case settingFill = "setting_fill"
+}
