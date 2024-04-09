c56254ad8989a2d7807aef12a4723c72b25e22ce
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-04-10 00:02:46 +0900

    build : Swinject 라이브러리 추가

--- Plugin/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
+++ Plugin/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
@@ -5,6 +5,7 @@ public extension TargetDependency {
 }
 
 public extension TargetDependency.SPM {
+    static let Swinject = TargetDependency.external(name: "Swinject")
 //    static let Alamofire = TargetDependency.external(name: "Alamofire")
 //    static let AlamofireImage = TargetDependency.external(name: "AlamofireImage")
 //    static let SkeletonUI = TargetDependency.external(name: "SkeletonUI")
