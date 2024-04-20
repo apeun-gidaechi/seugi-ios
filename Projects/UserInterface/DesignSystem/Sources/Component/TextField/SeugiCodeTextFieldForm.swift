d6a8d5e5227c1d1e38c19bdf482919466c510dda
bswlkh0310 <hhhello0507@gmail.com>
2024-04-20 10:29:50 +0900

    feat : EmailVerificationView 추가

---
+++ Projects/UserInterface/DesignSystem/Sources/Component/TextField/SeugiCodeTextFieldForm.swift
@@ -0,0 +1,44 @@
+//
+//  SeugiTextFieldForm.swift
+//  DesignSystem
+//
+//  Created by dgsw8th71 on 3/26/24.
+//  Copyright © 2024 seugi. All rights reserved.
+//
+
+import SwiftUI
+
+public struct SeugiCodeTextFieldForm: View {
+    
+    @Binding private var text: String
+    private var label: String
+    private var isForcedLabel: Bool
+    private let length: Int
+    
+    public init(
+        text: Binding<String>,
+        label: String,
+        isForcedLabel: Bool = true,
+        length: Int
+    ) {
+        self._text = text
+        self.label = label
+        self.isForcedLabel = isForcedLabel
+        self.length = length
+    }
+    
+    public var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            HStack(spacing: 2) {
+                Text(label)
+                    .font(.subtitle(.s2))
+                    .seugiColor(.sub(.black))
+                Text("*")
+                    .font(.subtitle(.s2))
+                    .seugiColor(.red(.r500))
+            }
+            .padding(.leading, 4)
+            SeugiCodeTextField(text: $text, length: length)
+        }
+    }
+}
