c2314515c44592985123d5f00751fc63bb96ec8d
bswlkh0310 <hhhello0507@gmail.com>
2024-04-25 21:37:37 +0900

    feat : create workspace interface

---
+++ Projects/Domain/WorkspaceDomain/Interface/UseCase/GetWorkspaces.swift
@@ -0,0 +1,3 @@
+public protocol GetWorkspaces {
+    func callAsFunction() -> [Workspace]
+}
---
+++ Projects/Domain/WorkspaceDomain/Sources/DataSource/WorkspaceDataSourceImpl.swift
@@ -0,0 +1,9 @@
+//
+//  WorkspaceDataSourceImpl.swift
+//  WorkspaceDomainInterface
+//
+//  Created by dgsw8th71 on 4/25/24.
+//  Copyright © 2024 apeun.gidaechi. All rights reserved.
+//
+
+import Foundation
