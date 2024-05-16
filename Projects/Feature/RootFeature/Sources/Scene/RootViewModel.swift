081bb022a646aaeef8f20477d299108d48d0f52e
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-05-16 09:10:02 +0900

    feat : Workspace Domain DI

--- Projects/Feature/RootFeature/Sources/Scene/RootViewModel.swift
+++ Projects/Feature/RootFeature/Sources/Scene/RootViewModel.swift
@@ -4,7 +4,12 @@ import WorkspaceDomainInterface
 
 public final class RootViewModel: ObservableObject {
     
-//    @Inject private var getWorkspace: any Workspace
+    @Inject private var getWorkspacesUseCase: any GetWorkspacesUseCase
     
     public init() {}
+    
+    @MainActor
+    func fetchWorkspaces() async {
+        
+    }
 }
