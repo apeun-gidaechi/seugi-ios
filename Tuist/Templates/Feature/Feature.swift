cdc314c1ade987483181eac440605734439e62e9
hhhello <hhhello0507@dgsw.hs.kr>
2024-07-02 14:22:20 +0900

    feat: create Module template

--- Tuist/Templates/Feature/Feature.swift
+++ Tuist/Templates/Feature/Feature.swift
@@ -1,18 +1,40 @@
 import ProjectDescription
 
-private let layerAttribute = Template.Attribute.required("layer")
 private let nameAttribute = Template.Attribute.required("name")
+private let root = "Projects/Feature/\(nameAttribute)Feature/Sources"
 
 private let template = Template(
     description: "A template for a new feature module",
     attributes: [
-        layerAttribute,
         nameAttribute
     ],
     items: [
-        .string(
-            path: "Projects/\(layerAttribute)/\(nameAttribute)\(layerAttribute)/Sources/Source.swift",
-            contents: "// made by tuist"
+        // MARK: - Sources
+        .file(
+            path: "\(root)/Assembly/\(nameAttribute)Assembly.swift",
+            templatePath: "FeatureAssembly.stencil"
+        ),
+        .file(
+            path: "\(root)/Factory/\(nameAttribute)FactoryImpl.swift",
+            templatePath: "FeatureFactoryImpl.stencil"
+        ),
+        .file(
+            path: "\(root)/Coordinator/\(nameAttribute)Coordinator.swift",
+            templatePath: "FeatureCoordinator.stencil"
+        ),
+        .file(
+            path: "\(root)/Scene/\(nameAttribute)View",
+            templatePath: "FeatureView.stencil"
+        ),
+        // MARK: - Interface
+        .file(
+            path: "Projects/Feature/\(nameAttribute)Feature/Interface/\(nameAttribute)Factory.swift",
+            templatePath: "InterfaceFactory.stencil"
+        ),
+        // MARK: - Example
+        .file(
+            path: "Projects/Feature/\(nameAttribute)Feature/Example/\(nameAttribute)Example.swift",
+            templatePath: "Example.stencil"
         )
     ]
 )
