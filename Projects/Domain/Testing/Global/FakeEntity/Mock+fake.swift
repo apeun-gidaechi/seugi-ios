cf2c5d51440202928d5a999ff539dcb7f4ae103d
hhhello <hhhello0507@dgsw.hs.kr>
2024-07-21 15:40:05 +0900

    fix: Entity+successResult

--- Projects/Domain/Testing/Global/FakeEntity/Mock+fake.swift
+++ Projects/Domain/Testing/Global/FakeEntity/Mock+fake.swift
@@ -4,17 +4,15 @@ import Foundation
 
 extension Entity {
     var successResult: APIResult<Self> {
-        [.success(self)].publisher
-            .receive(on: DispatchQueue.main)
-            .delay(for: .seconds(1), scheduler: DispatchQueue.main)
+        Just(.fetching)
+            .append(Just(.success(self)).delay(for: .seconds(1), scheduler: DispatchQueue.main))
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
     }
     
     var successBaseResult: APIResult<Base<Self>> {
-        [.success(self.withFakeBase())].publisher
-            .receive(on: DispatchQueue.main)
-            .delay(for: .seconds(1), scheduler: DispatchQueue.main)
+        Just(.fetching)
+            .append(Just(.success(withFakeBase())).delay(for: .seconds(1), scheduler: DispatchQueue.main))
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
     }
