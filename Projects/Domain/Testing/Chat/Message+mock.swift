1c75af9590312fb09eabe522b3b1281ea6d7e36d
hhhello <hhhello0507@dgsw.hs.kr>
2024-07-21 16:00:22 +0900

    chore: Message+mock random

--- Projects/Domain/Testing/Chat/Message+mock.swift
+++ Projects/Domain/Testing/Chat/Message+mock.swift
@@ -3,11 +3,11 @@ import Domain
 extension Message: Mock {
     public static func mock() -> Message {
         Message(
-            id: "",
-            chatRoomId: "",
+            id: .randomUUID(),
+            chatRoomId: .randomUUID(),
             type: .message,
-            userId: 0,
-            message: "wow 하이하이",
+            userId: .randomId(),
+            message: .randomUUID(),
             eventList: [],
             emoticon: nil,
             emojiList: [],
