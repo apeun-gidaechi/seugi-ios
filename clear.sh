9e4589e86f15dad20ed4c1bbcd14ac96c0d75789
hhhello <hhhello0507@dgsw.hs.kr>
2024-07-30 22:12:38 +0900

    build: Add .mise.toml

--- clear.sh
+++ clear.sh
@@ -1,4 +1,11 @@
 sudo rm -rf ~/Library/Developer/Xcode/DerivedData
 sudo rm -rf **/*.xcodeproj
 sudo rm -rf *.xcworkspace
-tuist clean
+
+# clean swift package
+rm -rf ~/Library/Caches/org.swift.swiftpm
+rm -rf ~/Library/org.swift.swiftpm
+
+rm -rf ./Tuist/Dependencies
+
+mise exec -- tuist clean
