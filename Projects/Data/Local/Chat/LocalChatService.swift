a80a35fc5f7f561221c9c6fb58c919d48bc4d8b5
hhhello <hhhello0507@dgsw.hs.kr>
2024-09-01 19:49:06 +0900

    Local codes

---
+++ Projects/Data/Local/Chat/LocalChatService.swift
@@ -0,0 +1,25 @@
+import Domain
+import RealmSwift
+
+final class LocalChatService: LocalChatRepo {
+    func insertGroup(workspaceId: String, room: Domain.Room) throws {
+        let realm = try Realm()
+        try realm.write {
+            realm.add(
+                LocalRoom.of(workspaceId: workspaceId, room: room)
+            )
+        }
+    }
+    
+    func getGroupAll(workspaceId: String) throws -> [Domain.Room] {
+        let realm = try Realm()
+        let rooms = realm.objects(LocalRoom.self)
+        return rooms.map { $0.toRoom() }
+    }
+    
+    func removeGroupAll() throws {
+        let realm = try Realm()
+        let rooms = realm.objects(LocalRoom.self)
+        realm.delete(rooms)
+    }
+}
