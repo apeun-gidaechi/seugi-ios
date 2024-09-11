5b26426cfa2721d29666eb9a54757a587b0bee62
hhhello <hhhello0507@dgsw.hs.kr>
2024-09-11 10:31:29 +0900

    HomeMealContainer

--- Projects/Feature/Sources/Home/HomeView.swift
+++ Projects/Feature/Sources/Home/HomeView.swift
@@ -51,7 +51,7 @@ public struct HomeView: View {
                     }
                     .applyAnimation()
                 todaySchedule
-                todayMeal
+                HomeMealContainer(for: viewModel.meals)
                 catSeugi
                 commingSchedule
                     .padding(.bottom, 80)
@@ -217,89 +217,6 @@ public struct HomeView: View {
             }
         }
         .matchedGeometryEffect(id: "todaySchedule", in: animation)
-    }
-    
-    @ViewBuilder
-    private var todayMeal: some View {
-        Group {
-            switch (flow, viewModel.meals) {
-            case (.failure, _), (_, .failure):
-                VStack(spacing: 12) {
-                    HStack(spacing: 8) {
-                        HeadlineIcon(icon: .utensilsLine)
-                        Text("오늘의 급식")
-                            .font(.subtitle(.s2))
-                            .seugiColor(.sub(.black))
-                        Spacer()
-                    }
-                    Text("학교를 등록하고 급식을 확인하세요")
-                        .seugiColor(.gray(.g600))
-                        .font(.body(.b2))
-                        .padding(.vertical, 12)
-                }
-                .applyCardEffect()
-            case (.success, .success(let data)):
-                VStack(spacing: 12) {
-                    HStack(spacing: 8) {
-                        HeadlineIcon(icon: .utensilsLine)
-                        Text("오늘의 급식")
-                            .font(.subtitle(.s2))
-                            .seugiColor(.sub(.black))
-                        Spacer()
-                        Image(icon: .expandRightLine)
-                            .resizable()
-                            .renderingMode(.template)
-                            .seugiColor(.gray(.g500))
-                            .frame(width: 24, height: 24)
-                    }
-                    HStack(spacing: 0) {
-                        VStack(alignment: .leading, spacing: 8) {
-                            Text("""
-                             오리훈제볶음밥
-                             간장두부조립
-                             배추김치
-                             초코첵스시리얼+우유
-                             오렌지
-                             """)
-                            .multilineTextAlignment(.leading)
-                            .font(.body(.b2))
-                            .seugiColor(.gray(.g700))
-                            Text("872kcal")
-                                .font(.caption(.c1))
-                                .seugiColor(.sub(.white))
-                                .padding(.horizontal, 8)
-                                .padding(.vertical, 4)
-                                .seugiBackground(.primary(.p500))
-                                .cornerRadius(100)
-                        }
-                        Spacer()
-                        VStack(spacing: 4) {
-                            Image(image: .apple1)
-                                .resizable()
-                                .frame(width: 94, height: 94)
-                            Text("아침")
-                                .font(.caption(.c1))
-                                .seugiColor(.gray(.g500))
-                        }
-                        .frame(maxWidth: .infinity)
-                    }
-                }
-                .applyCardEffect()
-            default:
-                VStack(spacing: 12) {
-                    HStack(spacing: 8) {
-                        HeadlineIcon(icon: .utensilsLine)
-                        Text("오늘의 급식")
-                            .font(.subtitle(.s2))
-                            .seugiColor(.sub(.black))
-                        Spacer()
-                    }
-                    ProgressView()
-                }
-                .applyCardEffect()
-            }
-        }
-        .matchedGeometryEffect(id: "todayMeal", in: animation)
     }
     
     @ViewBuilder
