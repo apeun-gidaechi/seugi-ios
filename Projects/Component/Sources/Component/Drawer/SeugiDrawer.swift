c8e541bf019d4d4b9967bf4752c7bc3345bb22d8
hhhello <hhhello0507@dgsw.hs.kr>
2024-09-15 14:04:33 +0900

    Apply drag drawer

--- Projects/Component/Sources/Component/Drawer/SeugiDrawer.swift
+++ Projects/Component/Sources/Component/Drawer/SeugiDrawer.swift
@@ -22,6 +22,7 @@ public extension View {
 public struct SeugiDrawerModifier<C>: ViewModifier where C: View {
     
     @Binding private var isOpen: Bool
+    @State private var translation: CGSize = .zero
     private var content: () -> C
     
     private let width: CGFloat = min(320, (UIApplication.shared.screen?.bounds.width ?? .infinity) - 80)
@@ -52,8 +53,25 @@ public struct SeugiDrawerModifier<C>: ViewModifier where C: View {
                         ? 0
                         : width
                     )
+                    .offset(x: max(translation.width, 0))
                     .animation(.default, value: isOpen)
                     .frame(maxWidth: .infinity, alignment: .trailing)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                self.translation = value.translation
+                            }
+                            .onEnded { value in
+                                // 절반 이상 움직였다면 drawer 닫기
+                                let translation = value.translation
+                                if translation.width >= self.translation.width / 2 {
+                                    isOpen = false
+                                }
+                                
+                                // 초기화
+                                self.translation = .zero
+                            }
+                    )
             }
     }
 }
