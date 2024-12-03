6368a34c23facb346a9d8198b6d15ea196f03e24
hhhello <hhhello0507@dgsw.hs.kr>
2024-12-04 08:08:01 +0900

    chore: make code pretty

--- Projects/Data/Remote/Core/JSONDecoderExt.swift
+++ Projects/Data/Remote/Core/JSONDecoderExt.swift
@@ -3,8 +3,8 @@ import DateUtil
 
 public extension JSONDecoder {
     static var myDecoder: JSONDecoder {
-        let d = JSONDecoder()
-        d.dateDecodingStrategy = .custom { decoder in
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .custom { decoder in
             let container = try decoder.singleValueContainer()
             let dateStr = try container.decode(String.self)
             
@@ -13,14 +13,12 @@ public extension JSONDecoder {
                 return Date.distantPast
             }
             
-            let dateFormatters = DateFormatterType.allCases
-            if let date = dateFormatters.compactMap({ DateFormatter(type: $0).date(from: dateStr) }).first {
-                return date
-            } else {
+            guard let date = DateFormatterType.allCases.compactMap({ DateFormatter(type: $0).date(from: dateStr) }).first else {
                 let context = DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Invalid date format")
                 throw DecodingError.dataCorrupted(context)
             }
+            return date
         }
-        return d
+        return decoder
     }
 }
